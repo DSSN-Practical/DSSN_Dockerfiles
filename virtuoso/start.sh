f97bc17bd268fc171f30ee9c6f84593d4db5ad4a
Markus Ackermann <neradis@fusionfactory.de>
2015-02-02 09:51:05 +0100

    modified start.sh scripts to allow for graceful stopping; xodx context context obtains virtodbc.so now from package manager

--- virtuoso/start.sh
+++ virtuoso/start.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-set -m
+# shutdown allowing for clean shutdown of daemons
+cleanup () {
+  echo "stopping nginx..."
+  service virtuoso-opensource-6.1 stop  
 
-tail -f -n0 /var/lib/virtuoso-opensource-6.1/db/virtuoso.log &
+  echo "stopping tail of log..."
+  [[ -n $tailpid ]] && kill "$tailpid"
+
+  exit 0
+}
+
+trap 'cleanup' INT TERM
 
 # start the virtuoso service
 echo "starting virtuoso..."
 service virtuoso-opensource-6.1 start
-echo "start finshed, tailing VOS log:"
-#bring tail for log outputs back to front
-fg
+
+echo "start finshed, tailing VOS log (press Ctrl + C to shut down this node):"
+tail -f /var/lib/virtuoso-opensource-6.1/db/virtuoso.log &
+tailpid=$!
+
+wait #on tail
--- xodx/start.sh
+++ xodx/start.sh
@@ -1,23 +1,35 @@
-#!/bin/sh
+#!/bin/bash
+
+# shutdown allowing for clean shutdown of daemons
+cleanup () {
+  echo "stopping nginx..."
+  service nginx stop
+  
+  echo "stopping php..." 
+  service php5-fpm stop
+  
+  echo "stopping tail of log..."
+  [[ -n $tailpid ]] && kill "$tailpid"
+
+  exit 0
+}
+
+trap 'cleanup' INT TERM
 
 # start the php5-fpm service
-echo "starting php …"
+echo "starting php..."
 service php5-fpm start
 
 # start the nginx service
-echo "starting nginx …"
+echo "starting nginx..."
 service nginx start
 
 echo "Xodx is ready to set sail!"
 cat /xodx-docker.fig
 echo ""
-echo "following log:"
+echo "following log (press Ctrl + C to shut down this node):"
 
-CACHEDIR=/var/www/cache/
-mkdir $CACHEDIR
-chmod a+w $CACHEDIR
+tail -f $LOG &
+tailpid=$!
 
-LOG="/var/www/xodx.log"
-touch $LOG
-chmod a+w $LOG
-tail -f $LOG
+wait #on tail
